7976734306ebbe145d3d55016d1e064009dd6e98
Mohsin Syed <smohsin@live.com>
2019-03-16 02:22:18 -0400

    all cases pass

--- Lisa.fs
+++ Lisa.fs
@@ -3,34 +3,25 @@ namespace HackerRank
 module Lisa =
 
    open System
-//    let txt = "10 5
-// 3 8 15 11 14 1 9 2 24 31"
-
-//    let txt = "5 3
-// 4 2 6 1 10"
-// -> 4
-
-   let txt = "90 29
-3 7 11 1 8 4 3 14 13 10 18 3 3 21 20 24 21 26 22 23 2 21 23 26 31 33 30 33 38 35 34 39 44 3 49 51 54 3 49 53 53 62 59 1 1 62 65 77 78 76 78 80 84 89 94 100 100 100 100 100 100 100 100 3 100 100 100 100 100 100 100 100 100 100 100 100 100 100 100 2 100 100 100 100 100 2 100 100 100 100"
-// -> 32
-
-   let stdin = new IO.StringReader(txt)
-   
+      
    let inline flip func a b = func b a 
    let (/<) a b = Math.Ceiling ((float)a / (float)b) |> int
    let (/~) a b = Math.Round ((float)a / (float)b) |> int
    let bint (v:bool) = Convert.ToInt32 (v)
 
    let specials max (ps, (s, e)) = 
-           let tp = 1 + e - s 
            let spc = (s-1) / max
            let ss = s + spc
-           let ee = spc * max
+           let ee = max + spc * max
            let mm = ss + bint(ss > ee)
-           if mm <= ps then 
-              1 + bint((ss % max) = 0)
-           else
-              0
+           
+           if max = 1 then
+               Math.Max((ps - (s - 1)), 0)
+           else 
+                if mm <= ps then 
+                    1 + bint(s < e && (ss % max) = 0)
+                else
+                    0
 
    let openChapters chapters max =
        chapters 
@@ -42,6 +33,7 @@ module Lisa =
        |> Seq.map (specials max)
 
    let (|Split|) (c:char) (txt:string) = txt.Split (c) |> Array.toList
+
 
    let main () = 
        let line1 = stdin.ReadLine()
@@ -55,3 +47,4 @@ module Lisa =
 
        let result = openChapters chapters k |> Seq.toList
        do stdout.WriteLine (result |> Seq.sum )
+      
